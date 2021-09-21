de4e76949627b02db8b38f843806ad342890329e
John Henry Krosby-Groner <jkrosby51@ccs.local>
2021-09-21 10:07:51 -0400

    edited DistanceRGB.py

--- DistanceRGB.py
+++ DistanceRGB.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
+import simpleio
 import time
 import board
 import adafruit_hcsr04
@@ -8,17 +9,30 @@ import neopixel
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
-rgbValue1 = 0
-rgbValue2 = 0
-
+blueValue = 0
+redValue = 0
+greenValue = 0
 
 while True:
     try:
-        print((sonar.distance,))
+        cm = sonar.distance
+        print((cm,))
+        if cm < 5:
+         dot.fill((0, 0, 0))
+        elif cm < 20:
+         redValue = simpleio.map_range(cm, 5, 20, 255, 0)
+         greenValue = 0
+         blueValue = simpleio.map_range(cm, 5, 20, 0, 255)
+         print("RGB: (", redValue, ", ", greenValue, ", ", blueValue, ")")
+         dot.fill((int(redValue), int(greenValue), int(blueValue)))
+        elif cm < 35:
+         redValue = 0
+         greenValue = simpleio.map_range(cm, 20, 35, 0, 255)
+         blueValue = simpleio.map_range(cm, 20, 35, 255, 0)
+         print("RGB: (", redValue, ", ", greenValue, ", ", blueValue, ")")
+         dot.fill((int(redValue), int(greenValue), int (blueValue)))
+        else:
+          dot.fill((0, 0, 0))
+        time.sleep(0.1)
     except RuntimeError:
         print("Retrying!")
-    dot.fill(colorwheel(rgbValue))
-    time.sleep(0.1)
-    if sonar.distance > 5 and sonar.distance < 20:
-     rgbValue1 = simpleio.map_range(sonar.distance, 0, 255, 5, 20)
-
