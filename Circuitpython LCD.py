470b41eb01c21fa3544a8ee4b7cb95235e4095b4
John Henry Krosby-Groner <jkrosby51@ccs.local>
2021-09-30 10:41:41 -0400

    added circuitpython lcd

--- Circuitpython LCD.py
+++ Circuitpython LCD.py
@@ -1,6 +1,14 @@
 import time
 import board
 import touchio
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+
+# get and i2c object
+i2c = board.I2C()
+
+# some LCDs are 0x3f... some are 0x27.
+lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
 
 touch_pad1 = board.A3
 touch_pad2 = board.A5
@@ -8,10 +16,36 @@ touch_pad2 = board.A5
 touch1 = touchio.TouchIn(touch_pad1)
 touch2 = touchio.TouchIn(touch_pad2)
 
-bool tOneOld = False
-bool tOneCurrent = False
-bool tTwoOld = False
-bool tTwoCurrent = False
+counter = 0
+photo1 = False
+state1 = False
+photo2 = False
+state2 = False
+direction = 1
 
 while True:
-    t1Old = t1Current
+###Cap Touch Counter###
+    photo1 = touch1.value
+    if photo1 and not state1:
+            counter += direction
+    state1 = photo1
+
+    photo2 = touch2.value
+    if photo2 and not state2:
+            direction *= -1;
+    state2 = photo2
+
+    print("Counter: ",counter, end = " \t")
+    print("Direction: ", direction)
+    #1time.sleep(0.1)
+
+###LCD###
+
+    if direction == 1:
+        lcd.print("Direction: Up  \n")
+    elif direction == -1:
+        lcd.print("Direction: Down\n")
+    lcd.print("Presses:   ")
+    lcd.print(str(counter))
+    lcd.print("  \n")
+
